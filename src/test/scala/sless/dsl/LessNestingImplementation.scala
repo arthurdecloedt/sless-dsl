eb150506e133b2c6132d9930eaa015d8d9158629
arthurdecloedt <arthurdecloedt@hotmail.com>
2019-12-20 16:42:10 +0100

    Removed irrelevant tests, final reformat, probably final commit

--- src/test/scala/sless/dsl/LessNestingImplementation.scala
+++
@@ -1,6 +0,0 @@
-package sless.dsl
-
-object LessNestingImplementation {
-  type DSL = PropertyDSL with NestedSelectorDSL with ValueDSL with Compilable
-  val dsl: DSL = ???
-}
--- src/test/scala/sless/dsl/MergeImplementation.scala
+++
@@ -1,6 +0,0 @@
-package sless.dsl
-
-object MergeImplementation {
-  type DSL = PropertyDSL with SelectorDSL with ValueDSL with MergeDSL with Compilable
-  val dsl: DSL = ???
-}
